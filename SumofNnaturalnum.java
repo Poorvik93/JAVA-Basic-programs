98d991be9941ff1ba25c7ece94ae7d766737d600
Poorvikgowda K <108992627+Poorvik93@users.noreply.github.com>
2023-07-19 23:18:28 +0530

    Add files via upload

---
+++ SumofNnaturalnum.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class SumofNnaturalnum {
+
+	public static void main(String[] args) {
+	  // Create a Scanner object to read user input
+		Scanner sc=new Scanner(System.in);
+		// take the user input
+		System.out.println("Enter the number ");
+		int number=sc.nextInt();
+//		call the method
+		SumofNnaturalnumApp.num(number);
+	}
+
+}
