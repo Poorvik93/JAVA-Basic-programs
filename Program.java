2685fd8606c38374a4dfb6eb4513d34fcc280533
Poorvikgowda K <108992627+Poorvik93@users.noreply.github.com>
2023-07-22 23:39:50 +0530

    Add files via upload

--- Program.java
+++ Program.java
@@ -2,11 +2,15 @@ import java.util.*;
 public class Program {
 
 	public static void main(String[] args) {
+		 // Create a Scanner object to read user input
 		Scanner sc=new Scanner(System.in);
+		// user input entering field
 		System.out.println("Enter the input");
 		int num=sc.nextInt();
+		// if num is greater then 1000=
 		if(num>100) {
 		ProgramApp number=new ProgramApp();
+//		Call the all methods present in ProgramApp class
 		number.isDivisibleByTwo(num);
 		number.isDivisibleByFive(num);
 		number.isDivisibleByThree(num);
--- ProgramApp.java
+++ ProgramApp.java
@@ -1,9 +1,9 @@
 
 public class ProgramApp {
-	// create the methode for isDivisibleByTwo
+	// create the method for isDivisibleByTwo
 	public void  isDivisibleByTwo(int num) {
 		System.out.println("the numbers is divisible by 2 are :" );
-	// 
+	// condition for checking isDivisibleByTwo
 		for (int i=1;i<=num;i++) {
 			if(i %2==0) {
 				System.out.print(i+ " ");
@@ -14,8 +14,11 @@ public class ProgramApp {
 
 	}
 	
+	// create the method for isDivisibleByThree
 	public void  isDivisibleByThree(int num) {
 		System.out.println("the numbers is divisible by 3 are :");
+		// condition for checking isDivisibleByThree
+
 		for (int i=1;i<=num;i++) {
 			if(i %3==0) {
 				System.out.print( i+ " ");
@@ -25,9 +28,10 @@ public class ProgramApp {
 		System.out.print( "\n ");
 
 	}
-	
+	// create the method for isDivisibleByFive
 	public void  isDivisibleByFive(int num) {
 		System.out.println("the numbers is divisible by 5 are :");
+		// condition for checking isDivisibleByFive
 		for (int i=1;i<=num;i++) {
 			if( i %5==0) {
 				System.out.print(i+ " ");
@@ -37,9 +41,11 @@ public class ProgramApp {
 		System.out.print( "\n ");
 
 	}
-	
+	// create the method for isDivisibleByTwoAndFive
+
 	public void  isDivisibleByTwoAndFive(int num) {
 		System.out.println("the numbers is divisible by 2 and 5 are :");
+		// condition for checking isDivisibleByTwoAndFive
 		for (int i=1;i<=num;i++) {
 			if(i%2==0 && i %5==0) {
 				System.out.print(i+ " ");
@@ -48,9 +54,10 @@ public class ProgramApp {
 		}
 		System.out.print( "\n ");
 	}
-	
+	// create the method for isDivisibleByThreeTable5
 	public void  isDivisibleByThreeTable5(int num) {
 		System.out.println("the numbers is divisible by 3 and comes in table of 5 :");
+		// condition for checking isDivisibleByThreeTable5
 		for (int i=1;i<=num;i++) {
 			if(i %3==0 && i%5==0) {
 				System.out.print(i + " ");
@@ -60,8 +67,11 @@ public class ProgramApp {
 		System.out.print( "\n ");
 
 	}
+	// create the method for primeNum
+
 	public void  primeNum(int num) {
 		System.out.println("Prime numbers are");
+		// condition for checking primeNum
 		for( int j=2;j<=num;j++) {
 			int count =0;
 			for (int i=1; i<=j;i++) {
@@ -74,9 +84,10 @@ public class ProgramApp {
 		}
 		System.out.println("\n");
 	}
-	
+	// create the method for evenDivisibleByThree
 	public void  evenDivisibleByThree(int num) {
 		System.out.println("even number divisuble by 3 are :");
+		// condition for checking evenDivisibleByThree
 		for (int i=1;i<=num;i++) {
 			if(i%2==0 && i%3==0) {
 				System.out.print(i + " ");
@@ -85,8 +96,10 @@ public class ProgramApp {
 		System.out.print( "\n ");
 
 	}
+	// create the method for oddDivisibleByFive
 	public void  oddDivisibleByFive(int num) {
 		System.out.println("odd number divisuble by 5 are :");
+		// condition for checking oddDivisibleByFive
 		for (int i=1;i<=num;i++) {
 			if(i%2!=0 && i%5==0) {
 				System.out.print(i + " ");
@@ -95,9 +108,11 @@ public class ProgramApp {
 		System.out.print( "\n ");
 
 	}
+	// create the method for evenPrimeNum
 	public void  evenPrimeNum(int num) {
 
 		System.out.print("Even Prime numbers are : ");
+		// condition for checking evenPrimeNum
 		for( int j=2;j<=num;j++) {
 			int count =0;
 			for (int i=1; i<=j;i++) {
@@ -111,10 +126,11 @@ public class ProgramApp {
 		System.out.println("\n");
 	
 	}
-	
+	// create the method for oddPrimeNum
 	public void  oddPrimeNum(int num) {
 
 		System.out.print("odd Prime numbers are :");
+		// condition for checking oddPrimeNum
 		for( int j=2;j<=num;j++) {
 			int count =0;
 			for (int i=1; i<=j;i++) {
