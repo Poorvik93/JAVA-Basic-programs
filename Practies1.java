d829d7e8f8219ecf1d07b8911ddb142972c3e200
Poorvikgowda K <108992627+Poorvik93@users.noreply.github.com>
2023-08-23 23:17:43 +0530

    Add files via upload

---
+++ Practies1.java
@@ -0,0 +1,177 @@
+package com.kodnest.practies;
+
+public class Practies1 {
+
+		public static void main(String[] args) {
+			
+	        // byte to byte ....
+	        byte a1 = 10;
+	        short b1;
+	        b1 = a1;
+	        System.out.println(a1);
+	        System.out.println(b1);
+	        System.out.println("byte to byte is  Casting is not required\n");
+
+	        // byte to short ....
+	        byte a2 = 10;
+	        short b2;
+	        b2 = a2;
+	        System.out.println(a2);
+	        System.out.println(b2);
+	        System.out.println("byte to short is implicit Casting\n");
+
+	        // byte to char....
+	        byte a3 = 10;
+	        char b3;
+	        b3 = (char) a3;
+	        System.out.println(a3);
+	        System.out.println(b3);
+	        System.out.println("byte to char is Explicit Casting\n");
+
+	        // byte to int ...
+	        byte a4 = 10;
+	        int b4;
+	        b4 = a4;
+	        System.out.println(a4);
+	        System.out.println(b4);
+	        System.out.println("byte to int is implicit Casting\n");
+
+	        // byte to long...
+	        byte a5 = 10;
+	        long b5;
+	        b5 = a5;
+	        System.out.println(a5);
+	        System.out.println(b5);
+	        System.out.println("byte to long is implicit Casting\n");
+
+	        // byte to float...
+	        byte a6 = 10;
+	        float b6;
+	        b6 = a2;
+	        System.out.println(a6);
+	        System.out.println(b6);
+	        System.out.println("byte to float is implicit Casting\n");
+
+	        // byte to double....
+	        byte a7 = 10;
+	        short b7;
+	        b7 = a2;
+	        System.out.println(a7);
+	        System.out.println(b7);
+	        System.out.println("byte to double is implicit Casting\n");
+	        
+	        // short to byte ....
+	        short a11 = 4;
+	        byte b11;
+	        b11 = (byte) a11;
+	        System.out.println(a11);
+	        System.out.println(b11);
+	        System.out.println("short to byte is Explicit Casting\n");
+
+	        // short to short ....
+	        short a12 = 10;
+	        short b12;
+	        b12 = a2;
+	        System.out.println(a12);
+	        System.out.println(b12);
+	        System.out.println("short to short is implicit Casting\n");
+
+	        // short to char....
+	        short a13 = 10;
+	        char b13;
+	        b13 = (char) a13;
+	        System.out.println(a13);
+	        System.out.println(b13);
+	        System.out.println("short to char is Explicit Casting\n");
+
+	        // short to int ...
+	        short a14 = 10;
+	        int b14;
+	        b14 = a14;
+	        System.out.println(a14);
+	        System.out.println(b14);
+	        System.out.println("short to int is implicit Casting\n");
+
+	        // short to long...
+	        short a15 = 10;
+	        long b15;
+	        b15 = a5;
+	        System.out.println(a5);
+	        System.out.println(b5);
+	        System.out.println("short to long is implicit Casting\n");
+
+	        // short to float...
+	        short a16 = 10;
+	        float b16;
+	        b16 = a12;
+	        System.out.println(a16);
+	        System.out.println(b16);
+	        System.out.println("short to float is implicit Casting\n");
+
+	        // short to double....
+	        short a17 = 10;
+	        short b17;
+	        b17 = a12;
+	        System.out.println(a17);
+	        System.out.println(b17);
+	        System.out.println("short to double is implicit Casting\n");
+	        
+	        
+	        // float to byte ....
+	        float a21 = 85.36f;
+	        byte b21;
+	        b21 = (byte) a21;
+	        System.out.println(a21);
+	        System.out.println(b21);
+	        System.out.println("float to byte is Explicit Casting\n");
+
+	        // float to short ....
+	        float a22 = 85.36f;
+	        short b22;
+	        b22 = (short) a22;
+	        System.out.println(a22);
+	        System.out.println(b22);
+	        System.out.println("float to short is Explicit Casting\n");
+
+	        // float to int....
+	        float a23 = 85.36f;
+	        int b23;
+	        b23 = (int) a23;
+	        System.out.println(a23);
+	        System.out.println(b23);
+	        System.out.println("float to int is Explicit Casting \n");
+
+	        // float to char ...
+	        float a24 = 85.36f;
+	        char b24;
+	        b24 = (char) a24;
+	        System.out.println(a24);
+	        System.out.println(b24);
+	        System.out.println("float to char is Explicit Casting\n");
+
+	        // float to long...
+	        float a25 = 85.36f;
+	        long b25;
+	        b25 = (long) a25;
+	        System.out.println(a25);
+	        System.out.println(b25);
+	        System.out.println("float to long is Explicit Casting\n");
+
+	        // float to float...
+	        float a26 = 85.36f;
+	        float b26;
+	        b26 = a26;
+	        System.out.println(a26);
+	        System.out.println(b26);
+	        System.out.println("float to float Casting is not required\n");
+
+	        // float to double....
+	        float a27 = 85.36f;
+	        double b27;
+	        b27 = a27;
+	        System.out.println(a27);
+	        System.out.println(b27);
+	        System.out.println("float to double is implicit Casting\n");
+	}
+
+}
