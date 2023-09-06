dc162099f11399df18c59f1edf3c902e6413edaa
Poorvikgowda K <108992627+Poorvik93@users.noreply.github.com>
2023-09-06 21:47:55 +0530

    FactorialNum_Recursion.java

---
+++ FactorialNum_Recursion.java
@@ -0,0 +1,15 @@
+public class FactorialNum_Recursion {
+    public static void main(String[] args) {
+        int number = 5; // Change this to the desired number
+        long factorial = calculateFactorial(number);
+        System.out.println("Factorial of " + number + " is " + factorial);
+    }
+
+    public static long calculateFactorial(int n) {
+        if (n == 0) {
+            return 1;
+        } else {
+            return n * calculateFactorial(n - 1);
+        }
+    }
+}
